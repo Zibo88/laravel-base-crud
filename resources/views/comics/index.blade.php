714831b34cc2b2cb8b70176cbfd81814b401443c
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-30 15:33:18 +0200

    stampo in pagina index la lista di tutti i prodotti

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>qui mostro la lista dei prodotti</h1>
+    <div class="container">
+        @foreach ($comics as $comic)
+            <div class="card">
+                <h1>{{$comic->title}}</h1>
+                <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+                <p>Descrizione:{{$comic->description}}</p>
+                <div>Prezzo: {{$comic->price}}</div>
+                <div>Data di uscita: {{$comic->sale_date}}</div>
+                <div>{{$comic->series}} - {{$comic->type}} </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
