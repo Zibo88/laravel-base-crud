4adebd69414a8ad41f2316931ea793101a44026d
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-30 15:57:40 +0200

    creato layout per mostrare la singola card

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,7 +5,14 @@
 @endsection
 
 @section('main_content')
-    
+<div class="card">
+    <h1>{{$comics->title}}</h1>
+    <img src="{{$comics->thumb}}" alt="{{$comics->title}}">
+    <p>Descrizione:{{$comics->description}}</p>
+    <div>Prezzo: {{$comics->price}}</div>
+    <div>Data di uscita: {{$comics->sale_date}}</div>
+    <div>{{$comics->series}} - {{$comics->type}} </div>
+</div>
      
    
 @endsection
