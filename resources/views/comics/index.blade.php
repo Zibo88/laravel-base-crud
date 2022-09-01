3cd62420a825ccb9551e3a35839acd5ea688259e
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-09-01 20:57:00 +0200

    install bootstrap, create colonne file index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,29 +6,39 @@
 
 @section('main_content')
     <div class="container">
-        @foreach ($comics as $comic)
-            <div class="card">
-                <h1>{{$comic->title}}</h1>
-                <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
-                <p>Descrizione:{{$comic->description}}</p>
-                <div>Prezzo: {{$comic->price}}</div>
-                <div>Data di uscita: {{$comic->sale_date}}</div>
-                <div>{{$comic->series}} - {{$comic->type}} </div>
-                {{-- Creo collegamento con il singolo prodotto --}}
-                {{-- la prima parte dell'url è il name che si trova in route:list, quello tra le [] è --}}
-                <a href="{{ route ('comics.show', ['comic' => $comic->id])}}">Mostra Dettagli</a>
-            </div>
-
-            {{-- Form pr cancellare il dato dalla riga --}}
-            <div>
-                <form action="{{route('comics.destroy', $comic->id)}}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    {{-- aggiungendo onClick chiediamo all'utente se davvero vuole cancellare l'elemento attraverso un alert --}}
-                    <input type="submit" value="Cancella" onClick="return confirm('Sei sicuro di voler cancellare elemento selezionato?');"> 
-                </form>
-            </div>
+        <div class="row row-cols-4 g-4">
+            @foreach ($comics as $comic)
+                <div class="col">
+                    <div class="card">
+                        <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$comic->title}}</h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{$comic->price}}</li>
+                            <li class="list-group-item">{{$comic->sale_date}}</li>
+                            <li class="list-group-item">{{$comic->type}}</li>
+                        </ul>
+                        <div class="card-body">
+                            {{-- reindirizzo l'utente alla pagina del singolo prodotto rispetto all'id --}}
+                            <a href="{{ route ('comics.show', ['comic' => $comic->id])}}" class="card-link">Mostra Dettagli</a>
+                            <a href="#" class="card-link"> 
+                                <div>
+                                    {{-- Form pr cancellare il dato dalla riga --}}
+                                    <form action="{{route('comics.destroy', $comic->id)}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        {{-- aggiungendo onClick chiediamo all'utente se davvero vuole cancellare l'elemento attraverso un alert --}}
+                                        <input type="submit" value="Cancella" onClick="return confirm('Sei sicuro di voler cancellare elemento selezionato?');"> 
+                                    </form>
+                                </div>
+                            </a>
+                        </div>
+                    </div> 
+                </div>
         @endforeach
+        </div>
+        
        
     </div>
 @endsection
